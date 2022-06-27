603d1cb7dc911487525321606ad173a89746f9cf
Rafael Santos <rafaelcs_94@hotmail.com>
2022-06-27 18:44:45 -0300

    feat: adds Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,17 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  constructor(private _lifePoints: number = 85, private _strength = 63) {}
+
+  get lifePoints(): number { return this._lifePoints; }
+
+  get strength(): number { return this._strength; }
+
+  receiveDamage(attackPoints: number) {
+    this._lifePoints -= attackPoints;
+    if (this.lifePoints <= 0) this._lifePoints = -1;
+    return this.lifePoints;
+  }
+
+  attack(enemy: SimpleFighter) { enemy.receiveDamage(this.strength); }
+}
