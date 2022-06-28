e289757b58337715242c2a8d491a266461234b50
Rafael Santos <rafaelcs_94@hotmail.com>
2022-06-28 15:59:04 -0300

    refactor: changes PVE battle class

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,6 +1,7 @@
 import Character from '../Character';
 import Fighter, { SimpleFighter } from '../Fighter';
 import Monster from '../Monster';
+import getRandomInt from '../utils';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
@@ -14,11 +15,11 @@ export default class PVE extends Battle {
   fight(): number {
     while (this._fighter.lifePoints > -1 
       && this._monsters[0].lifePoints > -1) {
-      this._fighter.attack(this._monsters[0]);
+      const random = getRandomInt(1, 2);
+      if (random === 1) this._fighter.attack(this._monsters[0]);
+      else this._fighter.special(this._monsters[0]);
       this.checkEnemiesHealth();
       this._monsters.forEach((enemy) => enemy.attack(this._fighter));
-      this._fighter.special(this._monsters[0]);
-      this.checkEnemiesHealth();
     }
     return super.fight();
   }
