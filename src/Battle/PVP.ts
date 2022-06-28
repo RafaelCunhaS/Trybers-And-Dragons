69cecb45dfb87d959d7c743bde3e34dad887fbba
Rafael Santos <rafaelcs_94@hotmail.com>
2022-06-28 11:12:54 -0300

    feat: adds PVP battle class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -4,19 +4,19 @@ import Battle from './Battle';
 
 export default class PVP extends Battle {
   constructor(
-    private fighter1: Fighter = new Character('Lutador 1'),
-    private fighter2: Fighter = new Character('Lutador 2'),
+    private _fighter1: Fighter = new Character('Lutador 1'),
+    private _fighter2: Fighter = new Character('Lutador 2'),
   ) {
-    super(fighter1);
+    super(_fighter1);
   }
 
   fight(): number {
-    while (this.fighter1.lifePoints > -1 && this.fighter2.lifePoints > -1) {
-      this.fighter1.attack(this.fighter2);
-      this.fighter2.attack(this.fighter1);
-      this.fighter2.special(this.fighter1);
-      this.fighter1.special(this.fighter2);
+    while (this._fighter1.lifePoints > -1 && this._fighter2.lifePoints > -1) {
+      this._fighter1.attack(this._fighter2);
+      this._fighter2.attack(this._fighter1);
+      this._fighter2.special(this._fighter1);
+      this._fighter1.special(this._fighter2);
     }
-    return this.fighter1.lifePoints === -1 ? -1 : 1;
+    return super.fight();
   }
 }
